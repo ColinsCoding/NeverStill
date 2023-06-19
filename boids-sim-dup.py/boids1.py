96b57ffc12518be568a56292604ed2400af9feab
Colin Casey <colincas37@gmail.com>
2023-06-18 22:31:43 -0700

    added pausing feature and about to add setting the mouse input as the avg position

--- boids-sim-dup.py/boids1.py
+++ boids-sim-dup.py/boids1.py
@@ -1,18 +1,18 @@
 import pygame
 import random
-import math
 import numpy as np
 
 # Parameters
 WIDTH, HEIGHT = 800, 600
 N = 50
-SPEED = 3
+SPEED = .025
 
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Boids Simulation")
 clock = pygame.time.Clock()
+font = pygame.font.Font(None, 74) # Font size 74
 
 # Boid class
 class Boid:
@@ -20,6 +20,7 @@ class Boid:
         self.position = np.array([random.uniform(0, WIDTH), random.uniform(0, HEIGHT)])
         self.velocity = np.array([random.uniform(-1, 1), random.uniform(-1, 1)]) * SPEED
 
+    # rule1: Boids try to fly towards the centre of mass of neighbouring boids.
     def rule1(self, boids):
         pcJ = np.array([0.0, 0.0])
         for b in boids:
@@ -32,10 +33,11 @@ class Boid:
         c = np.array([0.0, 0.0])
         for b in boids:
             if b != self:
-                if np.linalg.norm(b.position - self.position) < 100:
+                if np.linalg.norm(b.position - self.position) < 10:
                     c -= (b.position - self.position)
         return c
 
+    # rule3 is the same as rule1, but with velocity instead of position
     def rule3(self, boids):
         pvJ = np.array([0.0, 0.0])
         for b in boids:
@@ -51,27 +53,68 @@ class Boid:
         self.velocity += v1 + v2 + v3
         self.position += self.velocity
 
+        # Bouncing off boundaries
+        if self.position[0] < 0 or self.position[0] > WIDTH:
+            self.velocity[0] = -self.velocity[0]
+        if self.position[1] < 0 or self.position[1] > HEIGHT:
+            self.velocity[1] = -self.velocity[1]
+
     def draw(self):
-        pygame.draw.circle(screen, (255, 255, 255), (int(self.position[0]), int(self.position[1])), 5)
+        angle = np.arctan2(self.velocity[1], self.velocity[0])
+        points = [
+            (
+                self.position[0] + 10 * np.cos(angle),
+                self.position[1] + 10 * np.sin(angle)
+            ),
+            (
+                self.position[0] + 10 * np.cos(angle + 2.5),
+                self.position[1] + 10 * np.sin(angle + 2.5)
+            ),
+            (
+                self.position[0] + 10 * np.cos(angle - 2.5),
+                self.position[1] + 10 * np.sin(angle - 2.5)
+            ),
+        ]
+        pygame.draw.polygon(screen, (255, 255, 255), points)
+
+# Initialize boids
+boids = [Boid() for _ in range(N)]
 
 # Initialize boids
 boids = [Boid() for _ in range(N)]
 
 # Main loop
 running = True
+paused = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused # Toggle paused state
+    
     screen.fill((0, 0, 0))
 
-    # Move boids
+    # Always draw boids, regardless of paused state
     for boid in boids:
-        boid.move(boids)
         boid.draw()
 
+    # Only move boids if not paused
+    if not paused:
+        for boid in boids:
+            boid.move(boids)
+    else:
+        # Draw red overlay with "STOPPED" text if paused
+        overlay = pygame.Surface((WIDTH, HEIGHT))
+        overlay.fill((255, 0, 0))
+        overlay.set_alpha(128) # Setting opacity to 128 (range 0 to 255)
+        screen.blit(overlay, (0, 0))
+        text = font.render("STOPPED", 1, (255, 255, 255))
+        text_pos = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+        screen.blit(text, text_pos)
+
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(30)
 
 pygame.quit()
