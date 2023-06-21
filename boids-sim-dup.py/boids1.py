57f6e47ee0ac257cea5e89b9c5f4025f42ebd655
Colin Casey <colincas37@gmail.com>
2023-06-21 14:38:20 -0700

    added limiting velocity and kinda broke things come back later

--- boids-sim-dup.py/boids1.py
+++ boids-sim-dup.py/boids1.py
@@ -20,14 +20,20 @@ class Boid:
         self.position = np.array([random.uniform(0, WIDTH), random.uniform(0, HEIGHT)])
         self.velocity = np.array([random.uniform(-1, 1), random.uniform(-1, 1)]) * SPEED
 
-    # rule1: Boids try to fly towards the centre of mass of neighbouring boids.
-    def rule1(self, boids):
-        pcJ = np.array([0.0, 0.0])
-        for b in boids:
-            if b != self:
-                pcJ += b.position
-        pcJ /= (N-1)
-        return (pcJ - self.position) / 100
+    def limit_velocity(self, max_speed):
+        magnitude = np.linalg.norm(self.velocity)
+        if magnitude > max_speed:
+            self.velocity = (self.velocity / magnitude) * max_speed
+
+    def rule1(self, boids, mouse_pos):
+        perceived_center = np.array([0.0, 0.0])
+        for boid in boids:
+            if boid is not self:
+                perceived_center += boid.position
+        perceived_center += np.array(mouse_pos)
+        perceived_center /= len(boids)
+
+        return 0.02 * (perceived_center - self.position)
 
     def rule2(self, boids):
         c = np.array([0.0, 0.0])
@@ -37,7 +43,6 @@ class Boid:
                     c -= (b.position - self.position)
         return c
 
-    # rule3 is the same as rule1, but with velocity instead of position
     def rule3(self, boids):
         pvJ = np.array([0.0, 0.0])
         for b in boids:
@@ -47,17 +52,22 @@ class Boid:
         return (pvJ - self.velocity) / 8
 
     def move(self, boids):
-        v1 = self.rule1(boids)
+        v1 = self.rule1(boids, pygame.mouse.get_pos())
         v2 = self.rule2(boids)
         v3 = self.rule3(boids)
         self.velocity += v1 + v2 + v3
+        
+        # Limiting the velocity to a maximum speed
+        self.limit_velocity(max_speed=0.5)
+        
         self.position += self.velocity
 
-        # Bouncing off boundaries
+        # Bouncing off boundaries with dampening effect
+        dampening = 0.8  # The velocity is reduced to 80% of its original value after a bounce
         if self.position[0] < 0 or self.position[0] > WIDTH:
-            self.velocity[0] = -self.velocity[0]
+            self.velocity[0] = -self.velocity[0] * dampening
         if self.position[1] < 0 or self.position[1] > HEIGHT:
-            self.velocity[1] = -self.velocity[1]
+            self.velocity[1] = -self.velocity[1] * dampening
 
     def draw(self):
         angle = np.arctan2(self.velocity[1], self.velocity[0])
@@ -76,6 +86,7 @@ class Boid:
             ),
         ]
         pygame.draw.polygon(screen, (255, 255, 255), points)
+
 
 # Initialize boids
 boids = [Boid() for _ in range(N)]
